1bd964d8e4f427f1abf4be866cf08cda23e748d9
danilbogomolov <skyprodev1769@gmail.com>
2024-06-17 16:31:33 +0300

    initial commit

---
+++ src/main/java/com/test/ATOH/AtohApplication.java
@@ -0,0 +1,13 @@
+package com.test.ATOH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtohApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/ATOH/AtohApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.ATOH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtohApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
